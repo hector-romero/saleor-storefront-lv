a3bf97fd755bbaf7d52b2b0c05bddf1b70a8cddc
Hector Romero <hector.romero.rho@gmail.com>
2026-01-25 20:57:14 -0300

    Started translation

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -17,7 +17,7 @@ export async function Footer({ channel }: { channel: string }) {
 					// and use app token instead
 					Authorization: `Bearer ${process.env.SALEOR_APP_TOKEN}`,
 				},
-		  })
+			})
 		: null;
 	const currentYear = new Date().getFullYear();
 
@@ -73,7 +73,7 @@ export async function Footer({ channel }: { channel: string }) {
 					})}
 				</div>
 
-				{channels?.channels && (
+				{channels?.channels && channels?.channels.length > 1 && (
 					<div className="mb-4 text-neutral-500">
 						<label>
 							<span className="text-sm">Change currency:</span> <ChannelSelect channels={channels.channels} />
@@ -82,16 +82,7 @@ export async function Footer({ channel }: { channel: string }) {
 				)}
 
 				<div className="flex flex-col justify-between border-t border-neutral-200 py-10 sm:flex-row">
-					<p className="text-sm text-neutral-500">Copyright &copy; {currentYear} Your Store, Inc.</p>
-					<p className="flex gap-1 text-sm text-neutral-500">
-						Powered by{" "}
-						<Link target={"_blank"} href={"https://saleor.io/"}>
-							Saleor
-						</Link>{" "}
-						<Link href={"https://github.com/saleor/saleor"} target={"_blank"} className={"opacity-30"}>
-							<Image alt="Saleor github repository" height={20} width={20} src={"/github-mark.svg"} />
-						</Link>
-					</p>
+					<p className="text-sm text-neutral-500">Copyright &copy; {currentYear} VECTRA</p>
 				</div>
 			</div>
 		</footer>
--- src/ui/components/nav/components/NavLinks.tsx
+++ src/ui/components/nav/components/NavLinks.tsx
@@ -11,7 +11,7 @@ export const NavLinks = async ({ channel }: { channel: string }) => {
 
 	return (
 		<>
-			<NavLink href="/products">All</NavLink>
+			<NavLink href="/products">Todos</NavLink>
 			{navLinks.menu?.items?.map((item) => {
 				if (item.category) {
 					return (
