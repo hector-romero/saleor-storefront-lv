1b0b56f8481722e4554b7d46aef5144328be11d5
Hector Romero <hector.romero.rho@gmail.com>
2026-01-25 23:13:04 -0300

    Removed unsed imports

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import Image from "next/image";
+// import Link from "next/link";
+// import Image from "next/image";
 import { LinkWithChannel } from "../atoms/LinkWithChannel";
 import { ChannelSelect } from "./ChannelSelect";
 import { ChannelsListDocument, MenuGetBySlugDocument } from "@/gql/graphql";
